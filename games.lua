a37ae0128db2a78848351e099e396f39dea3f0dc
Sally <95383031+AtriumAdmin@users.noreply.github.com>
2021-12-18 15:40:21 +0000

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,65 @@
+-- Supported Games For Atrium, Some Scripts from https://www.rblx-scripts.com/ --
+ 
+EZHub - 204990346, 2629642516, 1962086868, 155615604, 2512643572, 3101667897, 402122991, 3823781113, 142823291, 2317712696, 171391948
+Factory Simulator - 6769764667
+Shindo Life - 4616652839
+BedWars - 6872265039
+Site-76 - 3057050847
+A Dumb Day - 5026999321
+Last Pirates - 6329844902
+RIOTFALL - 5595252083
+Right 2 Fight - 8170900938
+Tapping Mania - 5535087806
+Oil Warfare Tycoon - 4639625707
+Outlaster - 5343994291 (Premium)
+Boxing League - 3738091713
+Da Hood - 2788229376
+GG - 8052921123
+Lumber Tycoon - 13822889
+Funky Friday - 6447798030 (Premium)
+Barrier Remover - 1217895158, 3243063589, 718328620, 610172644, 738548299, 5580097107, 323925323, 3522042406, 2075082966
+Anomic - 4581966615
+Legends Of Speed - 3101667897 (Premium)
+Snow Shovelling Simulator - 1252559098
+Islands - 4872321990
+Project Hero - 6085581583
+Tower Defence Simulator - 3260590327 (Premium)
+Stroller - 1662219031
+Brookhaven RP - 4924922222
+Build A Boat For Treasure - 537413528
+King Legacy - 5931540094
+Blox Fruits - 7449423635
+KAT - 621129760
+Hotel Elephant - 23578803
+Happy Simulator - 6422697966
+Giant Simulator - 2986677229
+Grand Piece Online - 1730877806
+Demon Fall - 4855457388
+Bitcoin Miner - 2629642516
+Bad Business - 4996049426
+All Star Tower Defence - 4996049426
+Pet Simulator X - 6284583030
+Wizard Tycoon - 281489669 (Premium)
+Jailbreak - 606849621
+Break In - 3851622790
+Anime Fighters Simulator - 6299805723
+Stands Awakening - 5780309044
+Dragon Ball Rage - 71315343
+Elemental Battlegrounds - 566399244
+SharkBite - 734159876
+Zombie Attack - 1240123653, 1632210982
+Fencing - 5793474642, 12109643
+Stop It Slender! - 30869879
+Doom spire - 1215581239
+Anime Battle Arena - 1458767429 (Premium)
+Ragdoll Engine - 2041312716
+Dark Hub - 286090429, 2377868063, 263761432, 292439477, 3233893879, 3527629287, 2555870920, 5081773298
+Mad City - 1224212277 (Premium)
+My Restaurant - 4490140733
+D-DAY - 901793731
+Bee Simulator - 1537690962
+Ninja Legends - 3956818381
+Auto RB - 579955134
+AFS - 6299805723
+ 
+-- Supported Games For Atrium, Some Scripts from https://www.rblx-scripts.com/ --
